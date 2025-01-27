212922ae7e4a245511df67d53ab8dfcf407fe247
Azizkhan Myrzakassymov <a_myrzakassymov@kbtu.kz>
2025-01-27 16:17:24 +0500

    strings

---
+++ lab1/ex14.py
@@ -0,0 +1,9 @@
+# The upper() method returns the string in upper case:
+a = "Hello, World!"
+print(a.upper())
+# The lower() method returns the string in lower case:
+a = "Hello, World!"
+print(a.lower())
+# The split() method splits the string into substrings if it finds instances of the separator:
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
