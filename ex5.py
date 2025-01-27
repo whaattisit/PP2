3f7d6e30964f6e752d8435fadc2be1d611f37dbb
Azizkhan Myrzakassymov <a_myrzakassymov@kbtu.kz>
2025-01-27 12:20:20 +0500

    new!

---
+++ ex5.py
@@ -0,0 +1,14 @@
+myVariableName = "John" # Pascal case
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
