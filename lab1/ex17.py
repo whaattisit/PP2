58069e25ede7fbda5a623bec8b56d8e7a6eeb6a6
Azizkhan Myrzakassymov <a_myrzakassymov@kbtu.kz>
2025-01-27 16:26:15 +0500

    very new!

---
+++ lab1/ex17.py
@@ -0,0 +1,12 @@
+# The escape character allows you to use double quotes when you normally would not be allowed:
+txt = "We are the so-called \"Vikings\" from the north."
+
+# \'	Single Quote	
+# \\	Backslash	
+# \n	New Line	
+# \r	Carriage Return	
+# \t	Tab	
+# \b	Backspace	
+# \f	Form Feed	
+# \ooo	Octal value	
+# \xhh	Hex value
